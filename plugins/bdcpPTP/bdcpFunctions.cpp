ab971248aec1003f402a8d9ecd39f0f4eb2e011f
Jan-Rixt Van Hoye <jixt@burntide.eu>
2024-09-24 21:02:29 +0200

    Fixed BDCP GetThumbnail function
    When getting the thumbnail fails, the picture needs to be put to NULL.

--- plugins/bdcpPTP/bdcpFunctions.cpp
+++ plugins/bdcpPTP/bdcpFunctions.cpp
@@ -9,14 +9,16 @@
 #include "PictureList.h"
 #include "bdcpConfig.h"
 
-static BLooper* msgtarget = NULL;
-static PTPCamera* Camera  = NULL;
+static BLooper *msgtarget = NULL;
+static PTPCamera *Camera = NULL;
 
-int get_BDCP_API_Revision(void) {
+int get_BDCP_API_Revision(void)
+{
     return 2;
 }
 
-void getPluginVersion(version_info &ver) {
+void getPluginVersion(version_info &ver)
+{
     DEBUG("getPluginVersion\n");
     ver.major = 2;
     ver.middle = 0;
@@ -27,15 +29,18 @@ void getPluginVersion(version_info &ver) {
     sprintf(ver.long_info, "BDCP Plugin for PTP Cameras");
 }
 
-void getSupportedCameras(vector<string> & listofcams) {
+void getSupportedCameras(vector<string> &listofcams)
+{
     DEBUG("getSupportedCameras\n");
     listofcams.push_back("PTP Cameras (enhanced)");
 }
 
-status_t openCamera(void) {
+status_t openCamera(void)
+{
     int i = 20;
     StartPTP(&connectStateChanged);
-    while ((i > 0) && (!Camera || Camera->Status() != Ready)) {
+    while ((i > 0) && (!Camera || Camera->Status() != Ready))
+    {
         DEBUG("openCamera: waiting Camera to register - %i\n", i);
         snooze(100000);
         i--;
@@ -43,22 +48,26 @@ status_t openCamera(void) {
     return B_OK; // Return ok even if nothing connected. Use messaging for later connect.
 }
 
-status_t closeCamera(void) {
+status_t closeCamera(void)
+{
     DEBUG("closeCamera\n");
-    if ((Camera) && (Camera->Status() == Ready)) Camera->Disconnect();
+    if ((Camera) && (Camera->Status() == Ready))
+        Camera->Disconnect();
     return B_OK;
     StopPTP();
 }
 
-status_t getNumberofPics(int &number) {
+status_t getNumberofPics(int &number)
+{
 
     status_t status = B_ERROR;
     DEBUG("getNumerofPics\n");
     number = 0;
-    if ((Camera) && Camera->Status() == Identified) 
+    if ((Camera) && Camera->Status() == Identified)
         Camera->Connect();
-    
-    if ((Camera) && Camera->Status() == Ready) {
+
+    if ((Camera) && Camera->Status() == Ready)
+    {
         number = Camera->Pictures()->Count();
         status = B_OK;
     }
@@ -66,15 +75,21 @@ status_t getNumberofPics(int &number) {
     return status;
 }
 
-status_t setCurrentPicture(int picturenum) {
-    if ((Camera) && Camera->Status() == Ready) {
+status_t setCurrentPicture(int picturenum)
+{
+    if ((Camera) && Camera->Status() == Ready)
+    {
         DEBUG("Set current picture to %i of %i\n", picturenum, Camera->Pictures()->Count());
         return Camera->Pictures()->SetCurrent(picturenum);
-    } else return B_INTERRUPTED;
+    }
+    else
+        return B_INTERRUPTED;
 }
 
-void updateStatus(float bytesread) {
-    if (msgtarget) {
+void updateStatus(float bytesread)
+{
+    if (msgtarget)
+    {
         BMessage *msg = new BMessage(MSG_UPDATEBAR);
         msg->AddFloat("updateby", bytesread);
         msgtarget->PostMessage(msg);
@@ -82,54 +97,65 @@ void updateStatus(float bytesread) {
     }
 }
 
-status_t downloadPicture(BPath savedir) {
-    if (!Camera) return B_DEV_NOT_READY;
-	
-	PictureDesc* picture = Camera->Pictures()->CurrentItem();
-	
-	if (!picture) return B_BAD_INDEX;
+status_t downloadPicture(BPath savedir)
+{
+    if (!Camera)
+        return B_DEV_NOT_READY;
+
+    PictureDesc *picture = Camera->Pictures()->CurrentItem();
+
+    if (!picture)
+        return B_BAD_INDEX;
 
     DEBUG("downloadPicture %s to %s\n", picture->Name(), savedir.Path());
 
     return Camera->GetPicture(Camera->Pictures()->Current(), &savedir, (msgtarget) ? &updateStatus : NULL);
 }
 
-status_t deletePicture() {
+status_t deletePicture()
+{
     status_t status;
 
-	int picture = Camera->Pictures()->Current();
+    int picture = Camera->Pictures()->Current();
     DEBUG("Delete picture %i\n", picture);
 
     status = Camera->DeletePicture(picture);
     return status;
 }
 
-status_t takePicture() {
+status_t takePicture()
+{
     DEBUG("takePicture: Starting\n");
-    PTPCaptWindow* captWindow = new PTPCaptWindow(Camera);
-	if (captWindow->InitCheck() < B_OK) {
-		delete captWindow;
-		return B_NOT_ALLOWED;
-	}
-		
-	captWindow->Go();
+    PTPCaptWindow *captWindow = new PTPCaptWindow(Camera);
+    if (captWindow->InitCheck() < B_OK)
+    {
+        delete captWindow;
+        return B_NOT_ALLOWED;
+    }
+
+    captWindow->Go();
     return B_OK;
 }
 
-status_t getImageName(char* &name) {
-    if (!Camera) return B_DEV_NOT_READY;
-	PictureDesc* desc = Camera->Pictures()->CurrentItem();
-	if (desc) {
-		name = strdup(desc->Name());
-		DEBUG("getImageName: for picture %i yields %s\n", Camera->Pictures()->Current(), name);
-		return B_OK;
+status_t getImageName(char *&name)
+{
+    if (!Camera)
+        return B_DEV_NOT_READY;
+    PictureDesc *desc = Camera->Pictures()->CurrentItem();
+    if (desc)
+    {
+        name = strdup(desc->Name());
+        DEBUG("getImageName: for picture %i yields %s\n", Camera->Pictures()->Current(), name);
+        return B_OK;
     }
     return B_BAD_INDEX;
 }
 
-status_t getImageSize(int &size) {
-    PictureDesc* desc;
-    if ((Camera) && (desc = Camera->Pictures()->CurrentItem())) {
+status_t getImageSize(int &size)
+{
+    PictureDesc *desc;
+    if ((Camera) && (desc = Camera->Pictures()->CurrentItem()))
+    {
         size = desc->Size;
         DEBUG("getImageSize: for picture %d yields %" B_PRId32 "\n", Camera->Pictures()->Current(), desc->Size);
         return B_OK;
@@ -137,95 +163,122 @@ status_t getImageSize(int &size) {
     return B_ERROR;
 }
 
-status_t getImageDate(char* &date) {
-    PictureDesc* desc;
-    if ((Camera) && (desc = Camera->Pictures()->CurrentItem())) {
+status_t getImageDate(char *&date)
+{
+    PictureDesc *desc;
+    if ((Camera) && (desc = Camera->Pictures()->CurrentItem()))
+    {
         tm t = desc->Date;
         BString dateString;
         dateString.SetToFormat("%04i-%02i-%02i %02i:%02i:%02i", t.tm_year + 1900, t.tm_mon + 1, t.tm_mday + 1, t.tm_hour, t.tm_min, t.tm_sec);
         date = strdup(dateString);
         DEBUG("Get date of picture %i (%s)\n", Camera->Pictures()->Current(), date);
         return B_OK;
-    } else return B_ERROR;
+    }
+    else
+        return B_ERROR;
 }
 
-BWindow * configurePlugin(BPoint middle) {
+BWindow *configurePlugin(BPoint middle)
+{
     return (new PTPCnfWindow(middle));
 }
 
-status_t getThumbnail(BBitmap* &picture) {
-    PictureDesc* item;
-    if ((Camera) && (item = Camera->Pictures()->CurrentItem())) {
+status_t getThumbnail(BBitmap *&picture)
+{
+    PictureDesc *item;
+    if ((Camera) && (item = Camera->Pictures()->CurrentItem()))
+    {
 
         DEBUG("getThumbnail for picture %i\n", Camera->Pictures()->Current());
 
-        if (!item->thumb || !item->thumb->IsValid()) {
-            Camera->GetThumb(Camera->Pictures()->Current(), &(item->thumb));            
+        if (!item->thumb || !item->thumb->IsValid())
+        {
+            status_t status = Camera->GetThumb(Camera->Pictures()->Current(), &(item->thumb));
+            if (status < B_OK)
+            {
+                DEBUG("getThumbnail failed!");
+                picture = NULL;
+                return status;
+            }
+            else
+            {
+                DEBUG("Still OK yeah!?");
+            }
         }
         picture = item->thumb;
         return B_OK;
-    } else return B_ERROR;
+    }
+    else
+        return B_ERROR;
 }
 
-status_t getLevel3FunctionNames(map<char*, char*> &menunames) {
+status_t getLevel3FunctionNames(map<char *, char *> &menunames)
+{
     DEBUG("Get Level 3 Function names\n");
     menunames.clear();
     return B_NO_ERROR;
 }
 
-status_t setMessageTarget(BLooper* mtarget) {
+status_t setMessageTarget(BLooper *mtarget)
+{
     DEBUG("Set message target\n");
     msgtarget = mtarget;
     return B_NO_ERROR;
 }
 
-status_t getfiledata() {
+status_t getfiledata()
+{
     DEBUG("Get file data\n");
     return B_NO_ERROR;
 }
 
-status_t getDeviceType(int &deviceType) {
+status_t getDeviceType(int &deviceType)
+{
     deviceType = TYPE_USB;
     return B_OK;
 }
 
-status_t connectStateChanged(PTPCamera* camera) {
+status_t connectStateChanged(PTPCamera *camera)
+{
     Camera = camera;
-    if (msgtarget == NULL) return B_OK;
+    if (msgtarget == NULL)
+        return B_OK;
 
-    if (camera == NULL || camera->Status() == None) {
+    if (camera == NULL || camera->Status() == None)
+    {
         BMessage *notification = new BMessage(CAM_DISCONNECTED);
         msgtarget->PostMessage(notification);
-    } else {
-        switch(camera->Status()) {
-            case Identified : 
-                return B_OK;
-                break;
-            case Ready : {
-                BMessage *notification = new BMessage(CAM_CONNECTED);
-                notification->AddString("product", camera->GetProduct());
-                msgtarget->PostMessage(notification);
-                return B_OK;
-            }
-            default:
-                return B_ERROR;
+    }
+    else
+    {
+        switch (camera->Status())
+        {
+        case Identified:
+            return B_OK;
+            break;
+        case Ready:
+        {
+            BMessage *notification = new BMessage(CAM_CONNECTED);
+            notification->AddString("product", camera->GetProduct());
+            msgtarget->PostMessage(notification);
+            return B_OK;
+        }
+        default:
+            return B_ERROR;
         }
     }
     return B_ERROR;
 }
 
-status_t getImageHeight(int &height) {
+status_t getImageHeight(int &height)
+{
     height = Camera->Pictures()->CurrentItem()->YRes;
     return B_OK;
 }
 
-status_t getImageWidth(int &width) {
+status_t getImageWidth(int &width)
+{
     width = Camera->Pictures()->CurrentItem()->XRes;
     return B_OK;
 }
-
-
-
-
-
-
